0059bb8fe0d6473f7769830275e09561e848d6ed
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-07-02 13:42:19 +0200

    /

--- src/ItsToxicGG/Main.php
+++ src/ItsToxicGG/Main.php
@@ -1,1 +1,21 @@
+<?php
+namespace ItsToxicGG;
 
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use ItsToxicGG\Data;
+
+class Main extends PluginBase {
+  
+  /** @var Data */
+  public static $data;
+   
+  public function onEnable(): void{
+      $this->getLogger()->info("Enabled !");
+      $this->data = new Data($this);
+  }
+  
+  public static function getData(): Data{
+      return $this->data; 
+  }
+}
