ea95d8b03e0e46c7e6d9a5abbbcb60d81ed938ed
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-07-03 13:21:26 +0200

    Update Main.php

--- src/ItsToxicGG/Main.php
+++ src/ItsToxicGG/Main.php
@@ -9,13 +9,54 @@ class Main extends PluginBase {
   
   /** @var Data */
   public static $data;
+  
+	/** @var Main */
+	private static $instance;
+	
+	/** @var Provider */
+	private $provider;  
+  
+  public function onLoad(): void{
+     self::$instance = $this;  
+  }
    
   public function onEnable(): void{
       $this->getLogger()->info("Enabled !");
       $this->data = new Data($this);
+      $this->provider = new Provider($this);
+  }
+  
+  public static function getInstance(): Main{
+      return $this->instance; 
   }
   
   public static function getData(): Data{
       return $this->data; 
   }
-}
+    
+	public function getProvider(): Provider{
+		return $this->provider;
+	}
+	
+	private function setProvider(): void{
+		$providerName = $this->getConfig()->get("data-provider");
+		$provider = null;
+		switch(strtolower($providerName)){
+			case "yaml":
+				$provider = new YamlProvider();
+				$this->getLogger()->notice("YamlProvider successfully enabled.");
+				break;
+      case "mysql":
+        $provider = new MysqlProvider();
+			  $this->getLogger()->notice("MysqlProvider successfully enabled.");        
+        break;
+			default:
+				$this->getLogger()->error("Please set a valid data-provider in config.yml. Plugin Disabled");
+				$this->getServer()->getPluginManager()->disablePlugin($this);
+				break;
+		}
+		if($provider instanceof ProviderInterface){
+			$this->provider = $provider;
+		}
+	}
+}    
