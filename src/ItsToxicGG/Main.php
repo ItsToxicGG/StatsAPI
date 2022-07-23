9203da4104abb370c8e05ce9c067eb8e73308ede
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2022-07-23 20:10:45 +0200

    Better changes

--- src/ItsToxicGG/Main.php
+++ src/ItsToxicGG/Main.php
@@ -3,18 +3,21 @@ namespace ItsToxicGG;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
-use ItsToxicGG\Data;
+use ItsToxicGG\provider\{Provider, DefualtProvider, Database\MysqlProvider};
 
 class Main extends PluginBase {
   
-  /** @var Data */
-  public static $data;
-  
-	/** @var Main */
-	private static $instance;
+  /** @var MysqlProvider */
+  public static $mysqlpv;
 	
-	/** @var Provider */
-	private $provider;  
+  /** @var DefualtProvider */
+  public static $defualt;
+  
+  /** @var Main */
+  private static $instance;
+	
+  /** @var Provider */
+  private $provider;  
   
   public function onLoad(): void{
      self::$instance = $this;  
@@ -22,34 +25,46 @@ class Main extends PluginBase {
    
   public function onEnable(): void{
       $this->getLogger()->info("Enabled !");
-      $this->data = new Data($this);
-      $this->provider = new Provider($this);
+      $this->defualt = new DefualtProvider();
+      $this->mysqlpv = new MysqlProvider();
+      $this->provider = new Provider();
   }
   
   public static function getInstance(): Main{
       return $this->instance; 
   }
   
-  public static function getData(): Data{
-      return $this->data; 
+  public static function getMysqlProvider(): MysqlProvider{ // get mysql provider
+      return $this->mysqlpv; 
+  }
+	
+  public static function getDefualtProvider(): DefualtProvider{ // get yaml/defualt provider
+      return $this->defualt; 
   }
     
-	public function getProvider(): Provider{
-		return $this->provider;
-	}
+  public function getProvider(): Provider{ // get defualt or mysql provider by using example: $main->getProvider()->getMysql()->getKills($player); example end
+       return $this->provider;
+  }
 	
 	private function setProvider(): void{ // this is just a test mysql comming soon - Toxic
 		$providerName = $this->getConfig()->get("data-provider");
 		$provider = null;
 		switch(strtolower($providerName)){
-			case "yaml":
-				$provider = new YamlProvider();
-				$this->getLogger()->notice("YamlProvider successfully enabled.");
+			case "defualt":
+				$provider = new DefualtProvider();
+				$this->getLogger()->notice("YamlProvider/DefualtProvider successfully enabled.");
 				break;
-//                        case "mysql": soon
-//                           $provider = new MysqlProvider();
-// 			  $this->getLogger()->notice("MysqlProvider successfully enabled.");        
-//                            break;
+				
+			case "yaml":
+				$provider = new DefualtProvider();
+                                $this->getLogger()->notice("YamlProvider/DefualtProvider successfully enabled.");				
+				break;
+				
+			case "mysql":
+			   $provider = new MysqlProvider();
+			   $this->getLogger()->notice("MysqlProvider successfully enabled.");
+			   break;
+				
 			default:
 				$this->getLogger()->error("Please set a valid data-provider in config.yml. Plugin Disabled");
 				$this->getServer()->getPluginManager()->disablePlugin($this);
